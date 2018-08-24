067e5f7f3f80a062d70defcba07f939b4b2ddc21
Frédéric Gaudet <frederic.gaudet@isima.fr>
2018-08-24 08:24:36 +0200

    Use functions

--- build.sh
+++ build.sh
@@ -5,15 +5,19 @@ base=trusty
 arch=$1
 package=$2
 
-mkdir -p sandbox
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+asuser() { sudo su - "$1" -c "${*:2}"; }
+
+try mkdir -p sandbox
 
 if [ ! -d sandbox/$arch ]
 then
   echo "Starting debootstrap"
-  debootstrap --arch $arch $base sandbox/$arch http://archive.ubuntu.com/ubuntu
+  try debootstrap --arch $arch $base sandbox/$arch http://archive.ubuntu.com/ubuntu
 
   echo "Installing bashrc"
-
 cat > sandbox/$arch/etc/bash.bashrc << EOF
 export LANG=C
 export LC_ALL=C
@@ -35,15 +39,15 @@ EOF
 
 # Install buildpackages
 echo "Install Buildpackges"
-chroot sandbox/$arch apt-get update
-chroot sandbox/$arch apt-get install -y --force-yes dpkg-dev devscripts git-buildpackage debhelper libx11-dev libxtst-dev
+try chroot sandbox/$arch apt-get update
+try chroot sandbox/$arch apt-get install -y --force-yes dpkg-dev devscripts git-buildpackage debhelper libx11-dev libxtst-dev
 
 echo "Importing Kubuntu key"
-chroot sandbox/$arch apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2836CB0A8AC93F7A
+try chroot sandbox/$arch apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2836CB0A8AC93F7A
 
 echo "Generating locale"
-chroot sandbox/$arch locale-gen en_US.UTF-8
-chroot sandbox/$arch locale-gen fr_FR.UTF-8
+try chroot sandbox/$arch locale-gen en_US.UTF-8
+try chroot sandbox/$arch locale-gen fr_FR.UTF-8
 
 echo "Configure gbp"
 cat > sandbox/$arch/etc/git-buildpackage/gbp.conf << EOF
@@ -63,8 +67,8 @@ EOF
 fi
 
 echo "Mounting ..."
-mount -o bind /proc ./sandbox/$arch/proc/
-mount -o bind /dev ./sandbox/$arch/dev/
+try mount -o bind /proc ./sandbox/$arch/proc/
+try mount -o bind /dev ./sandbox/$arch/dev/
 
 
 # Install packages into sandbox
@@ -91,14 +95,14 @@ echo "Selected package=$package"
 # Build
 if [ $arch == "i386" ]
 then
-  DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LANG=en_US.UTF-8 chroot sandbox/$arch /bin/sh -c "cd sandbox/$package/$package;git-buildpackage -B"
+  DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LANG=en_US.UTF-8 try chroot sandbox/$arch /bin/sh -c "cd sandbox/$package/$package;git-buildpackage -B"
 else
-  DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LANG=en_US.UTF-8 chroot sandbox/$arch /bin/sh -c "cd sandbox/$package/$package;git-buildpackage"
+  DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LANG=en_US.UTF-8 try chroot sandbox/$arch /bin/sh -c "cd sandbox/$package/$package;git-buildpackage"
 fi
 
 #exit
-umount sandbox/$arch/proc/
-umount sandbox/$arch/dev/
+try mount sandbox/$arch/proc/
+try umount sandbox/$arch/dev/
 
 mv sandbox/$arch/sandbox/$package/build-area/* packages/$package/build-area/
 chown -R fred. packages/$package/build-area/*
